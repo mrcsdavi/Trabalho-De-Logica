e9949afc195b9b92203a203c09e323f10b42eb6a
Davi Sena <davilegalbr@gmail.com>
2025-07-30 01:25:10 -0300

    atualizando o mapeamento de simbolos

--- TrabalhoLogica.py
+++ TrabalhoLogica.py
@@ -20,30 +20,49 @@ class Npuzzle:
     ]    
 
     def MapeamentoSimbolos(): # função para mapear simbolos
-        estado = 1 # estado inicial
-        qtdSimbolos = 0 # 81 possibilidades
-        intMap = 0 # mapeamento dos simbolos para inteiros
+        
+        # qtdSimbolos = 0 # 81 possibilidades
+        intMap = 1 # mapeamento dos simbolos para inteiros
+            
+        for estado in range(20): # mapeando para n estados, que no caso escolhido são 20 aqui=
+            for posY in range(3): # posicao da coluna 
+                for posX in range(3): # posicao da linha
+                    for coluna in range(3): 
+                        for linha in range(3): 
+                            simbolos = f"{estado}_P_{posX+1}_{posY+1}_{Npuzzle.matrizInicial[linha][coluna]}" # criar simbolos
 
+                            Npuzzle.dictSimbolos[simbolos] = intMap # mapear para inteiros 
+                            intMap = intMap + 1
 
-        for coluna in range(3): 
-            for linha in range(3): 
-                intMap = intMap + 1
-
-                simbolos = f"{estado}_P_{linha+1}_{coluna+1}_{Npuzzle.matrizInicial[linha][coluna]}" # criar simbolos
-
-                Npuzzle.dictSimbolos[simbolos] = intMap # mapear para inteiros 
-
-                
+            
         for chave in Npuzzle.dictSimbolos:
-                print(chave, ":", Npuzzle.dictSimbolos[chave])     
+                print(chave, "->", Npuzzle.dictSimbolos[chave])  
 
-    def regrasEstado(): # regras para mapear cada estado
+    # def regrasEstado(): # regras para mapear cada estado
+    #     g = Glucose3        
 
-        pass
+    #     numPosicao = 0 
+    #     for i in Npuzzle.dictSimbolos:
+            
+    #         verificaPosicao = Npuzzle.dictSimbolos[numPosicao] # verificar p/ todos os 0, todos os 1, etc (Clausula A) 
+
+    #         for j in Npuzzle.dictSimbolos:
+    #                 for k in Npuzzle.dictSimbolos:
+    #                      simbolos = f"{estado}_P_{linha+1}_{coluna+1}_{Npuzzle.matrizInicial[linha][coluna]}" 
+                    
+                    
+    #                 percorreSimbolos = Npuzzle.dictSimbolos[i] # Vai percorrer todas as posições (clausula B) 
+
+    #                 g.add_clause([-verificaPosicao, -percorreSimbolos])
+                
+                    
+    #                 posicao = posicao + 1
+    #         verificaPosicao = Npuzzle.dictSimbolos[posicao]
 
 
 def main():
     Npuzzle.MapeamentoSimbolos()
+    #Npuzzle.regrasEstado()
 
 
 if __name__ == "__main__":
